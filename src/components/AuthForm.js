17d1fbd87bd71d8ee1f97844e90b98b3ac6992b9
mccedddy <cedricmacallan@gmail.com>
2024-08-08 16:50:43 +0800

    initialized tailwind and added styes to auth form

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -26,37 +26,59 @@ const AuthForm = ({ handleSignUp, handleLogIn }) => {
   };
 
   return (
-    <div>
-      <h1>{isLogin ? "Log In" : "Sign Up"}</h1>
-      <form onSubmit={handleSubmit}>
-        {!isLogin && (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="w-10/12 md:w-6/12 lg:w-3/12 border border-black flex flex-col justify-center items-center gap-2 py-4 px-6 rounded-lg">
+        <h1 className="text-2xl mb-2">{isLogin ? "Log In" : "Sign Up"}</h1>
+        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-2">
+          {!isLogin && (
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="border border-black rounded py-1 px-2"
+            />
+          )}
           <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
+            className="border border-black rounded py-1 px-2"
           />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="border border-black rounded py-1 px-2"
+          />
+          <button
+            type="submit"
+            className="border border-black rounded py-1 px-2 mt-2 bg-red-500"
+          >
+            {isLogin ? "Log In" : "Sign Up"}
+          </button>
+        </form>
+        {isLogin ? (
+          <div className="flex gap-1 mt-2">
+            <p className="text-xs">Don't have an account?</p>
+            <button onClick={toggleForm} className="underline text-xs">
+              Sign Up
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-1 mt-2">
+            <p className="text-xs">Already have an account?</p>
+            <button onClick={toggleForm} className="underline text-xs">
+              Log In
+            </button>
+          </div>
         )}
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">{isLogin ? "Log In" : "Sign Up"}</button>
-      </form>
-      <button onClick={toggleForm}>
-        {isLogin ? "Switch to Sign Up" : "Switch to Log In"}
-      </button>
+      </div>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = ({ user, handleLogOut }) => {
   return (
-    <div>
+    <div className="h-screen w-screen bg-red">
       <h1>Welcome, {user.email}</h1>
       <button onClick={handleLogOut}>Log Out</button>
     </div>
