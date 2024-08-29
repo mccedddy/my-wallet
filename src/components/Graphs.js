8e857b8c536946cc89752460ab3370b00129202b
mccedddy <cedricmacallan@gmail.com>
2024-08-29 15:30:34 +0800

    added graph filters and fixed color generation

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -7,6 +7,8 @@ import {
   getDocs,
   query,
   orderBy,
+  where,
+  limit,
 } from "firebase/firestore";
 import { Line } from "react-chartjs-2";
 import {
@@ -34,6 +36,13 @@ const Graphs = ({ user }) => {
   const [loading, setLoading] = useState(true);
   const [loadingRecords, setLoadingRecords] = useState(true);
 
+  const [filterType, setFilterType] = useState("Records"); // 'Records' or 'Date'
+  const [recordLimit, setRecordLimit] = useState("5");
+  const [dateRange, setDateRange] = useState({
+    from: new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
+    to: new Date(),
+  });
+
   // Fetch wallet names
   useEffect(() => {
     if (user?.email) {
@@ -51,10 +60,11 @@ const Graphs = ({ user }) => {
     }
   }, [user]);
 
-  // Fetch records based on wallet names
+  // Fetch records based on wallet names and filters
   useEffect(() => {
     if (user?.email && wallets.length > 0) {
       const fetchRecords = async () => {
+        let recordsQuery;
         const recordsCollectionRef = collection(
           db,
           "users",
@@ -62,17 +72,34 @@ const Graphs = ({ user }) => {
           "records"
         );
 
-        const recordsQuery = query(
-          recordsCollectionRef,
-          orderBy("timestamp", "asc")
-        );
+        // Filtering by Date
+        if (filterType === "Date") {
+          recordsQuery = query(
+            recordsCollectionRef,
+            where("timestamp", ">=", dateRange.from.getTime()),
+            where("timestamp", "<=", dateRange.to.getTime()),
+            orderBy("timestamp", "asc")
+          );
+          console.log("From", dateRange.from.getTime());
+          console.log("To", dateRange.to.getTime());
+        } else {
+          recordsQuery = query(
+            recordsCollectionRef,
+            orderBy("timestamp", "desc")
+          );
+          if (recordLimit !== "ALL") {
+            recordsQuery = query(recordsQuery, limit(parseInt(recordLimit)));
+          }
+        }
+
         const snapshot = await getDocs(recordsQuery);
 
-        const fetchedRecords = [];
+        let fetchedRecords = [];
 
         snapshot.forEach((doc) => {
           const data = doc.data();
           const { timestamp, description, id, ...walletBalances } = data;
+          console.log(timestamp);
 
           const hasRelevantWallets = Object.keys(walletBalances).some(
             (wallet) => wallets.includes(wallet)
@@ -100,6 +127,10 @@ const Graphs = ({ user }) => {
           }
         });
 
+        if (filterType === "Records" && fetchedRecords.length > 0) {
+          fetchedRecords.reverse();
+        }
+
         setRecords(fetchedRecords);
         setLoadingRecords(false);
       };
@@ -108,14 +139,32 @@ const Graphs = ({ user }) => {
     } else {
       setLoading(false);
     }
-  }, [user, wallets]);
+  }, [user, wallets, filterType, recordLimit, dateRange]);
+
+  // Colors
+  const colorPalette = [
+    "#FF0000",
+    "#FF7F00",
+    "#FFFF00",
+    "#00FF00",
+    "#0000FF",
+    "#9400D3",
+    "#f0289d",
+  ];
+
+  let usedColors = [];
 
   const getRandomColor = () => {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    if (colorPalette.length === usedColors.length) {
+      usedColors = [];
     }
+
+    let color;
+    do {
+      color = colorPalette[Math.floor(Math.random() * colorPalette.length)];
+    } while (usedColors.includes(color));
+
+    usedColors.push(color);
     return color;
   };
 
@@ -175,10 +224,65 @@ const Graphs = ({ user }) => {
 
   return (
     <div className="h-full w-full flex flex-col items-center">
-      {/* TODO: date filter */}
       {records.length > 0 && wallets.length > 0 ? (
         <div className="w-full flex flex-col items-center p-4 pt-2 mb-4 rounded-lg bg-background-light">
           <h1 className="m-1 text-lg">WALLET BALANCES OVER TIME</h1>
+          <div className="flex flex-wrap gap-2 w-full m-2">
+            <h1>Filter By:</h1>
+            <select
+              className="rounded w-22 text-text-dark bg-background outline-none"
+              value={filterType}
+              onChange={(e) => setFilterType(e.target.value)}
+            >
+              <option value="Records">Records</option>
+              <option value="Records">Date</option>
+              {/* TODO: fix date filter */}
+            </select>
+
+            {filterType === "Records" && (
+              <>
+                <h1>Show:</h1>
+                <select
+                  className="rounded w-22 text-text-dark bg-background outline-none"
+                  value={recordLimit}
+                  onChange={(e) => setRecordLimit(e.target.value)}
+                >
+                  <option value="ALL">ALL</option>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="50">50</option>
+                  <option value="100">100</option>
+                </select>
+              </>
+            )}
+
+            {filterType === "Date" && (
+              <div className="flex gap-2">
+                <h1>From:</h1>
+                <input
+                  type="date"
+                  className="rounded w-18 text-text-dark bg-background outline-none"
+                  value={dateRange.from.toISOString().split("T")[0]}
+                  onChange={(e) =>
+                    setDateRange({
+                      ...dateRange,
+                      from: new Date(e.target.value),
+                    })
+                  }
+                />
+                <h1>To:</h1>
+                <input
+                  type="date"
+                  className="rounded w-18 text-text-dark bg-background outline-none"
+                  value={dateRange.to.toISOString().split("T")[0]}
+                  onChange={(e) =>
+                    setDateRange({ ...dateRange, to: new Date(e.target.value) })
+                  }
+                />
+              </div>
+            )}
+          </div>
           <Line data={chartData} options={chartOptions} />
         </div>
       ) : loadingRecords ? (
