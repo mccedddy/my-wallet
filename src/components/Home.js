627f2b7206fafdc8e19d72c0eddb2b8b1eeeb671
mccedddy <cedricmacallan@gmail.com>
2024-10-13 21:53:41 +0800

    added tab icons, created empty owed tab

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,16 @@ import Records from "./Records";
 import Graphs from "./Graphs";
 import Wallets from "./Wallets";
 import Settings from "./Settings";
+import WalletIcon from "../assets/icons/walletLine.svg";
+import WalletIconDark from "../assets/icons/walletLineDark.svg";
+import RecordsIcon from "../assets/icons/table.svg";
+import RecordsIconDark from "../assets/icons/tableDark.svg";
+import SettingsIcon from "../assets/icons/settings.svg";
+import SettingsIconDark from "../assets/icons/settingsDark.svg";
+import GraphIcon from "../assets/icons/graph.svg";
+import GraphIconDark from "../assets/icons/graphDark.svg";
+import OwedIcon from "../assets/icons/owed.svg";
+import OwedIconDark from "../assets/icons/owedDark.svg";
 
 const Home = ({ user }) => {
   const [refresh, setRefresh] = useState(false);
@@ -45,40 +55,73 @@ const Home = ({ user }) => {
     <div className="h-full w-full flex flex-col flex-grow items-center">
       <Navbar username={username} />
       <div className="w-11/12 md:w-9/12 lg:w-8/12 flex flex-col">
-        <div className=" flex mt-4 mb-2 text-center border-b-2 border-background-light gap-5">
+        <div className="flex mt-4 mb-2 text-center border-b-2 border-background-light gap-5">
           <button
             onClick={() => toPage("records")}
-            className={`bg-background hover:text-text text-md sm:text-lg ${
+            className={`flex gap-2 justify-center items-center bg-background hover:text-text text-xl ${
               page === "records" ? "border-b-2 border-accent" : "text-text-dark"
             }`}
           >
-            RECORDS
+            <img
+              src={page === "records" ? RecordsIcon : RecordsIconDark}
+              alt="records icon"
+              className="w-6 h-6"
+            />
+            {page === "records" ? "RECORDS" : ""}
           </button>
           <button
             onClick={() => toPage("graphs")}
-            className={`bg-background hover:text-text text-md sm:text-lg ${
+            className={`flex gap-2 justify-center items-center bg-background hover:text-text text-xl ${
               page === "graphs" ? "border-b-2 border-primary" : "text-text-dark"
             }`}
           >
-            GRAPHS
+            <img
+              src={page === "graphs" ? GraphIcon : GraphIconDark}
+              alt="graph icon"
+              className="w-6 h-6"
+            />
+            {page === "graphs" ? "GRAPHS" : ""}
           </button>
           <button
             onClick={() => toPage("wallets")}
-            className={`bg-background hover:text-text text-md sm:text-lg ${
+            className={`flex gap-2 justify-center items-center bg-background hover:text-text text-xl ${
               page === "wallets"
                 ? "border-b-2 border-secondary"
                 : "text-text-dark"
             }`}
           >
-            WALLETS
+            <img
+              src={page === "wallets" ? WalletIcon : WalletIconDark}
+              alt="wallet icon"
+              className="w-6 h-6"
+            />
+            {page === "wallets" ? "WALLETS" : ""}
+          </button>
+          <button
+            onClick={() => toPage("owed")}
+            className={`flex gap-2 justify-center items-center bg-background hover:text-text text-xl ${
+              page === "owed" ? "border-b-2 border-accent" : "text-text-dark"
+            }`}
+          >
+            <img
+              src={page === "owed" ? OwedIcon : OwedIconDark}
+              alt="owed icon"
+              className="w-6 h-6"
+            />
+            {page === "owed" ? "OWED" : ""}
           </button>
           <button
             onClick={() => toPage("settings")}
-            className={`bg-background hover:text-text text-md sm:text-lg ${
+            className={`flex gap-2 justify-center items-center bg-background hover:text-text text-xl ${
               page === "settings" ? "border-b-2 border-text" : "text-text-dark"
             }`}
           >
-            SETTINGS
+            <img
+              src={page === "settings" ? SettingsIcon : SettingsIconDark}
+              alt="settings icon"
+              className="w-6 h-6"
+            />
+            {page === "settings" ? "SETTINGS" : ""}
           </button>
         </div>
         {page === "records" && (
@@ -86,6 +129,7 @@ const Home = ({ user }) => {
         )}
         {page === "graphs" && <Graphs user={user} />}
         {page === "wallets" && <Wallets user={user} />}
+        {/* {page === "owed" && <Owed user={user} />} */}
         {page === "settings" && (
           <Settings
             user={user}
