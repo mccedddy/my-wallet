a05f31398f10c6992cfa7f0655bcac111d5d45f7
mccedddy <cedricmacallan@gmail.com>
2024-11-05 19:05:23 +0800

    Refactored records page for table scroll

--- src/components/Records.js
+++ src/components/Records.js
@@ -189,121 +189,213 @@ const Records = ({ user, openCreateWallet }) => {
               DELETE RECORD
             </button>
           </div>
-          <table className="my-2 w-full text-xs sm:text-sm bg-background-light rounded-lg">
-            <thead>
-              <tr className="h-10">
-                <td className="w-16">DATE</td>
-                {wallets.map((wallet, index) => (
-                  <td key={wallet}>{wallet.toUpperCase()}</td>
-                ))}
-                <td className="w-24">TOTAL</td>
-                <td
-                  className={`w-12 bg-background-light rounded-lg ${
-                    modify ? "" : "hidden"
-                  }`}
-                ></td>
-              </tr>
-              <tr className="border-t-2 border-background"></tr>
-            </thead>
-            <tbody>
-              {currentRecords.map((record, index) => {
-                const globalIndex = (currentPage - 1) * recordsPerPage + index;
-                const previousRecord = records[globalIndex + 1] || {};
 
-                const currentTotal = wallets.reduce((acc, wallet) => {
-                  const balance = parseFloat(record.wallets[wallet] || 0);
-                  return acc + balance;
-                }, 0);
-
-                const previousTotal = wallets.reduce((acc, wallet) => {
-                  const prevBalance = parseFloat(
-                    previousRecord.wallets?.[wallet] || 0
-                  );
-                  return acc + prevBalance;
-                }, 0);
-
-                const totalDiff = currentTotal - previousTotal;
-                const totalDiffText =
-                  totalDiff > 0
-                    ? `(+${totalDiff.toFixed(0)})`
-                    : totalDiff < 0
-                    ? `(${totalDiff.toFixed(0)})`
-                    : "(+0)";
-
-                const totalDiffColor =
-                  totalDiff > 0
-                    ? "text-primary"
-                    : totalDiff < 0
-                    ? "text-accent"
-                    : "text-text-dark";
-
-                return (
-                  <React.Fragment key={index}>
-                    <tr className="h-8 text-center">
-                      {/* Upper part: date, wallet balances, total */}
-                      <td className="text-xs" rowSpan={2}>
+          <div className="w-full flex justify-center items-start mt-2">
+            {/* Date table */}
+            <table className="w-16 text-center">
+              <thead>
+                <tr>
+                  <th className="h-10 bg-background-light rounded px-2">
+                    DATE
+                  </th>
+                </tr>
+                <tr>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentRecords.map((record, index) => (
+                  <>
+                    <tr key={index}>
+                      <td className="h-16 bg-background-light rounded px-2">
                         <DateIcon
                           date={record.date}
                           index={index}
                           setHoveredRecord={setHoveredRecord}
                         />
                       </td>
-                      {wallets.map((wallet) => (
-                        <td key={wallet}>
-                          <div className="flex justify-center items-center gap-1">
-                            <h1>₱</h1>
-                            {record.wallets[wallet]
-                              ? record.wallets[wallet]
-                              : "0"}
+                    </tr>
+                    <tr>
+                      <td></td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+            </table>
+
+            {/* Wallets table */}
+            <div className="w-full overflow-x-auto scrollbar-hidden mx-1">
+              <table className="whitespace-nowrap w-full">
+                <thead>
+                  <tr>
+                    {wallets.map((wallet, index) => (
+                      <th
+                        key={wallet}
+                        className={`h-10 min-w-20 bg-background-light px-4 ${
+                          index === 0
+                            ? `rounded-l`
+                            : index === wallets.length - 1
+                            ? `rounded-r`
+                            : ``
+                        }`}
+                      >
+                        {wallet.toUpperCase()}
+                      </th>
+                    ))}
+                  </tr>
+                  <tr>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentRecords.map((record, index) => (
+                    <>
+                      <tr>
+                        {wallets.map((wallet, index) => (
+                          <td
+                            key={wallet}
+                            className={`h-8 min-w-24 bg-background-light px-4 text-center ${
+                              index === 0
+                                ? `rounded-tl`
+                                : index === wallets.length - 1
+                                ? `rounded-tr`
+                                : ``
+                            }`}
+                          >
+                            ₱ {record.wallets[wallet] || 0}
+                          </td>
+                        ))}
+                      </tr>
+                      <tr>
+                        <td
+                          colSpan={wallets.length}
+                          className="h-8 bg-background-light rounded-b px-4 text-left text-sm text-text-dark"
+                        >
+                          {hoveredRecord === index
+                            ? `${record.date} | ${record.time}`
+                            : record.description}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td></td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            {/* Total table */}
+            <table className="w-24 text-center">
+              <thead>
+                <tr>
+                  <th className="h-10 bg-background-light rounded px-4">
+                    TOTAL
+                  </th>
+                </tr>
+                <tr>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentRecords.map((record, index) => {
+                  const globalIndex =
+                    (currentPage - 1) * recordsPerPage + index;
+                  const previousRecord = records[globalIndex + 1] || {};
+
+                  const currentTotal = wallets.reduce((acc, wallet) => {
+                    const balance = parseFloat(record.wallets[wallet] || 0);
+                    return acc + balance;
+                  }, 0);
+
+                  const previousTotal = wallets.reduce((acc, wallet) => {
+                    const prevBalance = parseFloat(
+                      previousRecord.wallets?.[wallet] || 0
+                    );
+                    return acc + prevBalance;
+                  }, 0);
+
+                  const totalDiff = currentTotal - previousTotal;
+                  const totalDiffText =
+                    totalDiff > 0
+                      ? `(+${totalDiff.toFixed(0)})`
+                      : totalDiff < 0
+                      ? `(${totalDiff.toFixed(0)})`
+                      : "(+0)";
+
+                  const totalDiffColor =
+                    totalDiff > 0
+                      ? "text-primary"
+                      : totalDiff < 0
+                      ? "text-accent"
+                      : "text-text-dark";
+
+                  return (
+                    <>
+                      <tr key={index}>
+                        <td className="h-16 bg-background-light rounded px-4">
+                          <div className="flex flex-col items-center text-lg">
+                            <p className="text-nowrap">
+                              ₱ {currentTotal.toFixed(0)}
+                            </p>
+                            <p className={`text-xs ${totalDiffColor}`}>
+                              {totalDiffText}
+                            </p>
                           </div>
                         </td>
-                      ))}
-                      <td className="text-md">
-                        <div className="flex justify-center items-center gap-1">
-                          <h1>₱</h1>
-                          {currentTotal.toFixed(0)}
-                        </div>
-                      </td>
-                      <td
-                        onClick={() =>
-                          modify === "edit"
-                            ? handleEditRecord(record.id)
-                            : handleDeleteRecord(record.id)
-                        }
-                        className={`align-center cursor-pointer bg-background-light text-text-dark justify-center items-center py-2 font-bold ${
-                          modify ? "" : "hidden"
-                        }`}
-                        rowSpan={2}
-                      >
-                        <img
-                          src={modify === "edit" ? EditIcon : DeleteIcon}
-                          alt="trash"
-                          className="h-5 w-full flex justify-center"
-                        />
-                      </td>
-                    </tr>
-                    <tr className="h-8 text-center">
-                      {/* Lower part: description and change*/}
-                      <td
-                        colSpan={wallets.length}
-                        className="text-left text-text-dark px-2 text-xs"
-                      >
-                        {hoveredRecord === index
-                          ? `${record.date} | ${record.time}`
-                          : record.description}
-                      </td>
-                      <td>
-                        <span className={`text-xs ${totalDiffColor}`}>
-                          {totalDiffText}
-                        </span>
-                      </td>
-                    </tr>
-                    <tr className="border-b-2 border-background"></tr>
-                  </React.Fragment>
-                );
-              })}
-            </tbody>
-          </table>
+                      </tr>
+                      <tr>
+                        <td></td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            </table>
+
+            {/* Modify table */}
+            <table
+              className={`w-10 text-center ml-1 mx-0 ${modify ? "" : "hidden"}`}
+            >
+              <thead>
+                <tr>
+                  <th className="h-10 bg-background-light rounded"></th>
+                </tr>
+                <tr>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentRecords.map((record, index) => {
+                  return (
+                    <>
+                      <tr key={index}>
+                        <td
+                          onClick={() =>
+                            modify === "edit"
+                              ? handleEditRecord(record.id)
+                              : handleDeleteRecord(record.id)
+                          }
+                          className="h-16 align-center cursor-pointer bg-background-light justify-center items-center rounded px-3"
+                        >
+                          <img
+                            src={modify === "edit" ? EditIcon : DeleteIcon}
+                            alt="trash"
+                            className="min-h-5 min-w-5 flex justify-center"
+                          />
+                        </td>
+                      </tr>
+                      <tr>
+                        <td></td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+
+          {/* Pagination */}
           <div className="flex justify-center items-center gap-4 my-4">
             <button
               onClick={handlePrevPage}
@@ -325,6 +417,8 @@ const Records = ({ user, openCreateWallet }) => {
           </div>
         </div>
       )}
+
+      {/* Add record modal */}
       {showModal && (
         <Modal
           user={user}
