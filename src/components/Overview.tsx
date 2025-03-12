7ca50c733ea69d2cb29fc8953c6fcb4eeff1c615
mccedddy <cedricmacallan@gmail.com>
2025-03-12 18:44:57 +0800

    initialized supabase and sass, created home page, created header and overview components

---
+++ src/components/Overview.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Overview() {
+  return (
+    <div className='overview'>
+      <div className='overview-total'>
+          <p className='total-text'>Total</p>
+          <p className='total-amount'>P13,000</p>
+      </div>
+			<div className='overview-wallets'>
+				<div className='wallet'>
+					<p>Wallet 1</p>
+					<p>P13,000</p>
+				</div>
+				<div className='wallet'>
+					<p>Wallet 1</p>
+					<p>P13,000</p>
+				</div>
+				<div className='wallet'>
+					<p>Wallet 1</p>
+					<p>P13,000</p>
+				</div>
+			</div>
+			<p>Last Updated: 11/03/2025</p>
+    </div>
+  );
+}
+
+export default Overview;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Overview from '../components/Overview';
+
+function Home() {
+  return (
+    <div className='home'>
+      <Header />
+      <Overview />
+    </div>
+  );
+}
+
+export default Home;
